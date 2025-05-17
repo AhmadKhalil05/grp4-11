6548a4cd9db4ac559ccc1ec131f10160c0be5ddc
AhmadKhalil05 <s12323576@stu.najah.edu>
2025-05-17 19:43:18 +0300

    Added the DiningTable Model

--- app/Models/DiningTable.php
+++ app/Models/DiningTable.php
@@ -6,5 +6,10 @@ use Illuminate\Database\Eloquent\Model;
 
 class DiningTable extends Model
 {
-   //
+    protected $table = 'dining_table'; 
+    protected $primaryKey = 'table_id';
+    public $timestamps = false;
+
+    protected $fillable = ['restaurant_id', 'seating_capacity','number', 'location', 'status'];
 }
+

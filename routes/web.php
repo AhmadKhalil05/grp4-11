864bc0fb8336b296592731cef0f1b57f604ce815
Ruaa_Saleh <s12220826@stu.najah.edu>
2025-05-18 18:44:12 +0300

    Update web.php and added the users routes

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,12 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\RestaurantManager\TableController;
+use App\Http\Controllers\Auth\AuthController;
 
+Route::get('/users', [AuthController::class,'index'])->name('users.index');
+Route::post('/users', [AuthController::class,'store'])->name('users.store');
+Route::put('/users{id}' ,[AuthController::class,'update'])->name('users.update');
+Route::delete('/users{id}' ,[AuthController::class,'destroy'])->name('users.destroy');
 Route::get('/dining-table', [TableController::class, 'indexView'])->name('dining-table.index');
 Route::post('/dining-table', [TableController::class, 'store'])->name('dining-table.store');
 Route::put('/dining-table/{id}', [TableController::class, 'update'])->name('dining-table.update');
